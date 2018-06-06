34f3d4e6313f3b2cdfbc1c8dc4d09120bf247fd1
KraProgrammer <m.kranzer@hotmail.com>
2018-06-06 17:45:38 +0200

    start adding review

--- src/Controllers/Review.php
+++ src/Controllers/Review.php
@@ -10,6 +10,8 @@ class Review extends \Framework\Controller {
     $this->authenticationManager = $authenticationManager;
   }
 
+  const REV_ID = 'rid';
+
   public function GET_Index() {
     if (!$this->authenticationManager->isAuthenticated()) {
       return $this->redirect('LogIn', 'User'); // TODO add context so that user acan return here after loggging in
@@ -20,4 +22,26 @@ class Review extends \Framework\Controller {
         'reviews' => $this->authenticationManager->isAuthenticated() ?  $this->dataLayer->getReviewForUserId($this->authenticationManager->getAuthenticatedUser()->getId()) : null
       ));
   }
+
+  public function GET_Create() {
+    // $product = $this->hasParam(self::REV_ID) ? $this->dataLayer->getProductsForId($this->getParam(self::REV_ID)) : null;
+
+    // if ($product !== null) {
+    //   $category = $product->getCategory();
+    //   $name = $product->getName();
+    //   $manufacturer = $product->getManufacturer();
+    // } else {
+    //   $category = '';
+    //   $name= '';
+    //   $manufacturer= '';
+    }
+
+    $this->renderView('CreateProduct', array(
+      'user' => $this->authenticationManager->getAuthenticatedUser(),
+      'product' => $product,
+      'category' => $category,
+      'name' => $name,
+      'manufacturer' => $manufacturer     
+    ));
+  }  
 }
